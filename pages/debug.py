0c4a00857eeaf461e24c56c165ddb10e8fdd8cf0
daniil <danilawower@gmail.com>
2023-02-20 02:59:49 +0300

    marshal project

--- pages/debug.py
+++ pages/debug.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = webdriver.ChromeOptions()
 prefs = {"download.default_directory" : "C:\\Users\daniil\\PycharmProjects\\automation_qa_course\\downloads"}
 options.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options = options)
 
 driver.get('https://samplelib.com/ru/sample-mp4.html')
 download = driver.find_element(By.XPATH, "//div[@class='items']//div[1]//div[1]//div[4]//a[1]")
@@ -17,9 +17,3 @@ download.click()
 time.sleep(5)
 driver.close()
 
-#@pytest.fixture(scope="function")
-#def driver():
-    #driver = webdriver.Chrome(ChromeDriverManager().install())
-   # driver.maximize_window()
-    #yield driver
-    #driver.quit()
