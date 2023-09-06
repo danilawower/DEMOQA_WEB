b184998e99db979b3cef3ea27705713c1cc1fabb
danilawower <92742319+danilawower@users.noreply.github.com>
2023-09-06 16:31:56 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,14 +9,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 
-
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument('--incognito')
+    driver = webdriver.Chrome()
     driver.maximize_window()
-    yield driver
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f'Screenshot {datetime.today()}', attachment_type=allure.attachment_type.PNG)
+    yield driver
+    driver.close()
     driver.quit()
 
 
