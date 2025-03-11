3a47698bf9f381be411a1284581f59d54ad206a5
danilawower <92742319+danilawower@users.noreply.github.com>
2025-03-11 18:26:17 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -14,7 +14,6 @@ def driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--ignore-certificate-errors")
-    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-cache")
     driver = webdriver.Chrome()
     driver.maximize_window()
